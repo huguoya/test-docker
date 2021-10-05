5ecf61e2094e13f73a8831812f6d24b3eb6ea382
huguoya <huguoya@gmail.com>
2021-10-05 20:28:01 +0800

    create

--- src/main/java/com/huguoya/testdocker/TestDockerApplication.java
+++ src/main/java/com/huguoya/testdocker/TestDockerApplication.java
@@ -1,17 +1,21 @@
 package com.huguoya.testdocker;
 
 import com.huguoya.testdocker.configuration.YmlConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
+@Slf4j
 public class TestDockerApplication {
 
     public static void main(String[] args) {
-
-        ConfigurableApplicationContext context=  SpringApplication.run(TestDockerApplication.class, args);
-        System.out.println("http://localhost:"+context.getBean(YmlConfig.class).getPort()+"/doc.html");
+        log.info("project start" + LocalDateTime.now());
+        ConfigurableApplicationContext context = SpringApplication.run(TestDockerApplication.class, args);
+        System.out.println("http://localhost:" + context.getBean(YmlConfig.class).getPort() + "/doc.html");
     }
 
 }
